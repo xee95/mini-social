22c6abe17d82dd325e62f978b061160c52bb0abb
Zeeshan Nawaz <35934083+xee95@users.noreply.github.com>
2025-05-14 03:46:37 +0500

    error remove

--- app/(main)/edit-post/[id]/page.tsx
+++ app/(main)/edit-post/[id]/page.tsx
@@ -1,11 +1,9 @@
 import EditPostClient from "./components/edit-post-client";
-
-// Server component that passes the ID to the client component
-export default async function EditPostPage({
-  params,
-}: {
+interface PageProps {
   params: { id: string };
-}) {
+}
+// Server component that passes the ID to the client component
+export default async function EditPostPage({ params }: PageProps) {
   const { id } = await params;
   return <EditPostClient postId={id} />;
 }
