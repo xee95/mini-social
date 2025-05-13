670c2f9cf9f16ea1a41409f7fa5f9fbfee76f2ef
Zeeshan Nawaz <35934083+xee95@users.noreply.github.com>
2025-05-14 03:52:02 +0500

    final

--- app/(main)/edit-post/[id]/page.tsx
+++ app/(main)/edit-post/[id]/page.tsx
@@ -1,9 +1,12 @@
+import { use } from "react";
 import EditPostClient from "./components/edit-post-client";
-interface PageProps {
-  params: { id: string };
-}
+
 // Server component that passes the ID to the client component
-export default async function EditPostPage({ params }: PageProps) {
-  const { id } = params;
+export default async function EditPostPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = use(params);
   return <EditPostClient postId={id} />;
 }
